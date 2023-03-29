6f3f4e837ac1192597c5f4d9af7d7ee79ad5aee2
Aluel-Kuot <maryaluelkuot@gmail.com>
2023-03-29 19:18:29 +0300

    functions

---
+++ math.py
@@ -0,0 +1,18 @@
+def add(a,b):
+    answer=a+b
+    return answer
+    
+def subtract(c,d):
+    remainder=c-d
+    return remainder 
+    
+def multiply(e,f):
+     product =e*f  
+     return product
+def divide(g,h):
+    remainder=g/h
+    return remainder    
+def modulus(i,j):
+    remain=i%j
+    return remain
+
